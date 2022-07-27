baa9d1ffbdd888d301bf77390f0f33827f39e6c2
AHpx <ahpx@yandex.com>
2022-07-27 09:06:38 +0800

    Initialized

---
+++ NugetPubTest/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace NugetPubTest;
+
+public class Class1
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Substract(int a, int b)
+    {
+        return a - b;
+    }
+}
